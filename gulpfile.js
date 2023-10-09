677e3da1fcd98f01c6e78acba52ab8eb89a0ae88
Vince Mendella <vmendella@gmail.com>
2023-10-09 09:22:35 -0500

    project setup and intro

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const bs = require('browser-sync').create();
+
+// Sass Task
+function scssTask() {
+  return src('app/scss/style.scss', { sourcemaps: true })
+  .pipe(sass())
+  .pipe(postcss([autoprefixer(), cssnano]))
+  .pipe(dest('dist', { sourcemaps: '.'}));
+}
+
+// Browsersync Task
+function browsersyncServe(cb) {
+  bs.init({
+    server: '.'
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  bs.reload()
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch('*.html', browsersyncReload);
+  watch('app/scss/**/*.scss',
+  series(scssTask, browsersyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, browsersyncServe, watchTask);
+exports.build = scssTask;
