5897469c995d03f724f47126205d82b957f02656
Vince Mendella <vmendella@gmail.com>
2024-09-02 10:41:29 -0500

    add missing js attributes - gulp-babel, gulp-terser and jsTask watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,24 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
 const bs = require('browser-sync').create();
 
 // Sass Task
 function scssTask() {
   return src('app/scss/style.scss', { sourcemaps: true })
   .pipe(sass())
-  .pipe(postcss([autoprefixer(), cssnano]))
+  .pipe(postcss([autoprefixer(), cssnano()]))
   .pipe(dest('dist', { sourcemaps: '.'}));
+}
+
+// Javascript Task
+function jsTask() {
+  return src('app/js/script.js', { sourcemaps: true})
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // Browsersync Task
@@ -28,11 +38,13 @@ function browsersyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-  watch('*.html', browsersyncReload);
-  watch('app/scss/**/*.scss',
-  series(scssTask, browsersyncReload));
+  watch ('*.html', browsersyncReload);
+  watch ['app/scss/**/*.scss', 'app/**/*.js'],
+  series (scssTask, jsTask, browsersyncReload);
 }
 
 // Default Gulp Task
-exports.default = series(scssTask, browsersyncServe, watchTask);
-exports.build = scssTask;
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask, jsTask);
