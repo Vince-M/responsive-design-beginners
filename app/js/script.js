03ccaa911b221c24e628e2dab5be88eac884692b
Vince Mendella <vmendella@gmail.com>
2024-09-02 10:43:48 -0500

    add aria & animation to js file for mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,31 @@
-console.log('Hello World!');
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const menuTopNav = document.querySelector('#menuTopNav');
+const breakpoint = window.matchMedia('(width < 43.75em)');
+
+
+if (breakpoint.matches) {
+  console.log('is mobile');
+  menuTopNav.setAttribute('inert', '');
+}
+else {
+  console.log('is table/desktop');
+  menuTopNav.removeAttribute('inert');
+}
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+function openMobileMenu() {
+  console.log('run openMobileMenu');
+  btnOpen.setAttribute('aria-expanded', 'true');
+  menuTopNav.removeAttribute('inert');
+}
+
+function closeMobileMenu() {
+  console.log('run, closeMobileMenu');
+  btnOpen.setAttribute('aria-expanded', 'false');
+  menuTopNav.setAttribute('inert', '');
+}
+
+console.log(breakpoint);
---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";var btnOpen=document.querySelector("#btnOpen"),btnClose=document.querySelector("#btnClose");function openMobileMenu(){console.log("run openMobileMenu"),btnOpen.setAttribute("aria-expanded","true")}function closeMobileMenu(){console.log("run, closeMobileMenu"),btnOpen.setAttribute("aria-expanded","false")}btnOpen.addEventListener("click",openMobileMenu),btnClose.addEventListener("click",closeMobileMenu);
+//# sourceMappingURL=script.js.map
